3d382a774c17596fb26cea165980732900224610
Ian.Lintner <Ian.Lintner@WebFilings.com>
2014-03-19 12:02:56 -0500

    Added recipe for mysql drupal database creation

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,30 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+#
+# Cookbook Name:: database
+# Recipe:: import
+#
+# Copyright 2012, Adam Brett. All Rights Reserved.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+include_recipe "database::mysql"
+
+# Store this in a variable so we don't keep repeating it
+mysql_connection_info = {
+    :host => "localhost",
+    :username => 'root',
+    # automatically get this from the override_attributes call!
+    :password => ''
+}
+
+# my_database = database name
+mysql_database 'drupal' do
+  connection mysql_connection_info
+  action :create
+end
